e52aec1440efa58eda116974dd09cefcc3782f90
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-03-21 17:16:34 +0100

    media queries

---
+++ jour06/job04/index.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen">
+    <script type="text/javascript" src="script.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+  <body>
+    <div>
+      <section class="section">blo</section>
+      <section class="section">bli</section>
+      <section class="section">ki</section>
+      <section class="section">kbhjh</section>
+    </div>
+  </body>
+</html>
