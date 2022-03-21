8882f5e220fe04271f30a8fdcf54ef119333cd0a
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-03-21 14:44:26 +0100

    media queries

---
+++ jour06/job02/index.php
@@ -0,0 +1,40 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen">
+    <script type="text/javascript" src="script.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" methode = "post" id="myForm">
+  <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Enter a name" name="name" id="name">
+
+    <label for="surname"><b>Surname</b></label>
+    <input type="text" placeholder="Enter a surname" name="surname" id="surname" >
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" id="email">
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" id="psw" >
+    <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat">
+
+    <button type="button" id="signupbtn" name="myBtn">Sign Up</button>
+    
+  </div>
+</form>
+</body>
+</html>
