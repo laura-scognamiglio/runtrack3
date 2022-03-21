4480e095a5ea4ca74affcdc11366ee83dc56ad2b
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-03-21 12:22:31 +0100

    media queries

--- jour06/job01/index.php
+++ jour06/job01/index.php
@@ -5,6 +5,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="style1.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="style2.css" rel="stylesheet" type="text/css" media=" screen and (min-width:1680px) and (max-width:1920px)">
+    <link href="style3.css" rel="stylesheet" type="text/css" media="screen and (min-width:1280px) ">
+    <link href="style4.css" rel="stylesheet" type="text/css" media="screen and (max-width:1280px)">
     <script type="text/javascript" src="script.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
